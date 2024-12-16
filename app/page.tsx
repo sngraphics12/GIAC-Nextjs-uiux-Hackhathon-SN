fe9971145b48a06eb470668e2808f91d4ddaf175
SN-Graphics <sgraphics989@gmail.com>
2024-12-16 19:50:25 +0500

    Completed

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+
+
+import DontMiss from "@/components/DONTMISS";
+import Essential from "@/components/ESSENTIAL";
+import Featured from "@/components/FEATURED";
+import GearUp from "@/components/GEARUP";
+import Hero from "@/components/HERO";
+import Navigation from "@/components/Navigation";
+import { CarouselSize } from "@/components/BEST"
+
+
+export default function Home() {
+  return (
+  <main>
+ 
+    <Hero/>
+    <CarouselSize/>
+    <Featured/>
+    <GearUp/>
+    <DontMiss/>
+    <Essential/>
+    <Navigation/>
+  </main>
+  )
+}
